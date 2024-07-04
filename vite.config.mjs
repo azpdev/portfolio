9a36b9ee87d6b066b157ab81962f77c8460c8818
Alex Palomin <alexpalomin21@gmail.com>
2024-07-04 14:40:16 -0600

    Fix Resume Routing.

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,17 +2,19 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import htmlMinifier from "vite-plugin-html-minifier";
 
+const build = {
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, "index.html"),
+      resume: resolve(__dirname, "resume", "index.html"),
+    },
+  },
+};
+
 export default defineConfig({
   appType: "mpa",
   base: "",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        resume: resolve(__dirname, "resume", "index.html"),
-      },
-    },
-  },
+  build,
   plugins: [
     htmlMinifier({
       minify: true,
