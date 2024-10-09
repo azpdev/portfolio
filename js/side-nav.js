d1e0efc3ccc953eef23a7feb98b995eb0fcdf0e6
Alex Palomin <alexpalomin21@gmail.com>
2024-10-09 15:54:18 -0600

    Added backdrop to side nav with dismiss on click.

--- js/side-nav.js
+++ js/side-nav.js
@@ -1,12 +1,26 @@
-window.addEventListener("load", () => {
-  document.getElementById("open-nav-btn").addEventListener("click", () => {
-    document.getElementById("my-side-nav").style.width = "min(15rem, 100vw)";
-    document.getElementById("my-side-nav").style.borderLeft =
-      "4px solid var(--color-primary)";
-  });
+function closeSideNav() {
+  document.getElementById("side-nav").style.width = "0";
+  document.getElementById("side-nav").style.borderLeft = "none";
+  document.getElementById("side-nav-back").style.height = "0";
+  document.getElementById("side-nav-back").style.width = "0";
+}
 
-  document.getElementById("close-nav-btn").addEventListener("click", () => {
-    document.getElementById("my-side-nav").style.width = "0";
-    document.getElementById("my-side-nav").style.borderLeft = "none";
-  });
+function openSideNav() {
+  document.getElementById("side-nav").style.width = "min(15rem, 100vw)";
+  document.getElementById("side-nav").style.borderLeft =
+    "4px solid var(--color-primary)";
+  document.getElementById("side-nav-back").style.height = "100vh";
+  document.getElementById("side-nav-back").style.width = "100vw";
+}
+
+window.addEventListener("load", () => {
+  document
+    .getElementById("open-nav-btn")
+    .addEventListener("click", openSideNav);
+  document
+    .getElementById("close-nav-btn")
+    .addEventListener("click", closeSideNav);
+  document
+    .getElementById("side-nav-back")
+    .addEventListener("click", closeSideNav);
 });
