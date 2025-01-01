4e7b9bd988fd04c091aa08ae1678d67a91543cde
Alex Palomin <alexpalomin21@gmail.com>
2025-01-01 12:31:15 -0700

    Added vscode config, eslint, prettier, and husky.

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+export default [
+  {
+    files: ["**/*.js", "**/*.cjs", "**/*.mjs"],
+    ignores: ["**/node_modules/**", "**/dist/**"],
+    rules: {
+      "prefer-const": "error",
+      "no-constant-binary-expression": "error",
+      "no-constant-condition": "error",
+      "capitalized-comments": "error",
+      "no-multi-spaces": "error",
+      "no-multiple-empty-lines": "error",
+      "no-trailing-spaces": "error",
+      "no-irregular-whitespace": "error",
+      camelcase: "error",
+      eqeqeq: "error",
+      "no-else-return": "error",
+      "no-empty-function": "error",
+      "no-magic-numbers": "error",
+      "no-unused-expressions": "error",
+    },
+  },
+];
